210722952305ee2205a414154ed2b5ad20e673c0
Zahraa <zahraa.yasser.2006@gmail.com>
2025-11-01 15:18:09 +0200

    Add project files.

---
+++ task2 ieee/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task2_ieee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+            Console.WriteLine("Enter 5 integers:");
+            for (int i = 0; i < 5; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+            int largest = numbers[0];
+            for (int i = 1; i < 5; i++)
+            {
+                if (numbers[i] > largest)
+                {
+                    largest = numbers[i];
+                }
+            }
+            Console.WriteLine("The largest number is: " + largest);
+
+        }
+    }
+}
